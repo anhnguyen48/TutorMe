13920fde0344b406e516e7954105a59e27465867
Yovel Glisko <yglisko@falcon.bentley.edu>
2021-04-04 18:41:39 -0400

    Create basic email layout and button

--- Application/app/src/main/java/com/example/tutormeapp/Email.java
+++ Application/app/src/main/java/com/example/tutormeapp/Email.java
@@ -1,6 +1,27 @@
 package com.example.tutormeapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Email extends AppCompatActivity {
+public class Email extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.email);
+        Button sendButton = (Button) findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(this);
+    }
+    public void onClick (View v) {
+        switch (v.getId()) {
+            case R.id.sendButton:
+                Intent i1= new Intent(this, MainActivity.class);
+                startActivity(i1);
+                break;
+        }
+    }
 }
