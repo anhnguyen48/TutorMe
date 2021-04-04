8643fdf6fd7d8a600485cbb8a03702d2cf03cfa5
Yovel Glisko <yglisko@falcon.bentley.edu>
2021-04-04 18:35:55 -0400

    Create login page with basic functionality and add login button tester

--- Application/app/src/main/java/com/example/tutormeapp/Login.java
+++ Application/app/src/main/java/com/example/tutormeapp/Login.java
@@ -1,6 +1,27 @@
 package com.example.tutormeapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        Button loginButton = (Button) findViewById(R.id.loginScreenButton);
+        loginButton.setOnClickListener(this);
+    }
+    public void onClick (View v) {
+        switch (v.getId()) {
+            case R.id.loginScreenButton:
+                Intent i1= new Intent(this, MainActivity.class);
+                startActivity(i1);
+                break;
+        }
+    }
 }
