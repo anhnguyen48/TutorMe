cf66462428f1cd7bc153486dc621564a57e42dfe
YovelGlisko <72568312+YovelGlisko@users.noreply.github.com>
2021-04-28 02:23:41 -0400

    Make email work

--- Application/app/src/main/java/com/example/tutormeapp/Email.java
+++ Application/app/src/main/java/com/example/tutormeapp/Email.java
@@ -1,14 +1,18 @@
 package com.example.tutormeapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Email extends AppCompatActivity implements View.OnClickListener{
-
+    private EditText address;
+    private EditText subject;
+    private EditText body;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +23,14 @@ public class Email extends AppCompatActivity implements View.OnClickListener{
     public void onClick (View v) {
         switch (v.getId()) {
             case R.id.sendButton:
-                Intent i1= new Intent(this, MainActivity.class);
+                EditText address = findViewById(R.id.address);
+                EditText subject = findViewById(R.id.subject);
+                EditText body = findViewById(R.id.body);
+                Intent i1= new Intent(Intent.ACTION_SENDTO);
+                i1.setData(Uri.parse("mailto:"));
+                i1.putExtra(i1.EXTRA_EMAIL, new String[]{address.getText().toString()});
+                i1.putExtra(i1.EXTRA_SUBJECT, subject.getText().toString());
+                i1.putExtra(i1.EXTRA_TEXT, body.getText().toString());
                 startActivity(i1);
                 break;
         }
