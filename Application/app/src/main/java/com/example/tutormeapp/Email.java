31940b0b4e0d2374f73ae344d98461e1338e6b3b
anhnguyen48 <48160479+anhnguyen48@users.noreply.github.com>
2021-04-28 03:22:41 -0400

    Update Email
    I messed up while push and pull. Should be okay now

--- Application/app/src/main/java/com/example/tutormeapp/Email.java
+++ Application/app/src/main/java/com/example/tutormeapp/Email.java
@@ -1,28 +1,43 @@
+
 package com.example.tutormeapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Email extends AppCompatActivity implements View.OnClickListener{
 
+    private EditText address;
+    private EditText subject;
+    private EditText body;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.email);
-        Button sendButton = (Button) findViewById(R.id.sendButton);
-        sendButton.setOnClickListener(this);
+
     }
     public void onClick (View v) {
         switch (v.getId()) {
             case R.id.sendButton:
-                //after user sends the email, take user back to Main Activity
-                Intent i1= new Intent(this, MainActivity.class);
+                address = findViewById(R.id.address);
+                subject = findViewById(R.id.subject);
+                body = findViewById(R.id.body);
+                Intent i1 = new Intent(Intent.ACTION_SENDTO);
+                i1.setData(Uri.parse("mailto:"));
+                i1.putExtra(i1.EXTRA_EMAIL, new String[]{address.getText().toString()});
+                i1.putExtra(i1.EXTRA_SUBJECT, subject.getText().toString());
+                i1.putExtra(i1.EXTRA_TEXT, body.getText().toString());
                 startActivity(i1);
                 break;
         }
     }
 }
+
+
+
